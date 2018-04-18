3a83883d9061cd1a0cf0d5acc0700b078fc2a337
Dan Smith <dsmith@pivotal.io>
2018-04-17 18:29:37 -0700

    Updating tampermonkey script for concourse

--- src/esheep.user.js
+++ src/esheep.user.js
@@ -2,20 +2,21 @@
 // @name         web eSheep
 // @namespace    http://esheep.petrucci.ch/
 // @version      0.3
-// @description  Add a sheep on your Google and Bing page!
+// @description  Add three sheep on concourse
 // @author       Adriano
-// @include      http*://*.google.*/*
-// @include      http*://*.bing.*/*
+// @include      https://concourse.apachegeode-ci.info/*
 // @icon         http://esheep.petrucci.ch/favicon.gif
 // @grant        none
+// @run-at       document-idle
 // ==/UserScript==
 
 (function() {
     'use strict';
-    window.addEventListener("load", function() {
       var s = document.createElement("script");
-      s.addEventListener("load", function() {var sheep = new eSheep(); sheep.Start("https://adrianotiger.github.io/web-esheep/src/animation.xml"); });
-      s.setAttribute("src", "https://adrianotiger.github.io/web-esheep/src/esheep.js");
+      s.addEventListener("load", function() {var sheep = new eSheep(); sheep.Start("https://upthewaterspout.github.io/web-esheep/src/animation.xml"); });
+      s.addEventListener("load", function() {var sheep = new eSheep(); sheep.Start("https://upthewaterspout.github.io/web-esheep/src/animation.xml"); });
+      s.addEventListener("load", function() {var sheep = new eSheep(); sheep.Start("https://upthewaterspout.github.io/web-esheep/src/animation.xml"); });
+      s.setAttribute("src", "https://upthewaterspout.github.io/web-esheep/src/esheep.js");
       document.getElementsByTagName('head')[0].appendChild(s);
-    });
+      document.getElementsByTagName('rect').borderTopStyle='solid';
 })();
